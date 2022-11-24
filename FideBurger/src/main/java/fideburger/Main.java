b729c298f42ea948020728bc43afeab1ddb33dee
Sofía Mora Muñoz <72779933+sofiamm@users.noreply.github.com>
2022-11-23 21:39:51 -0600

    update main

--- FideBurger/src/main/java/fideburger/Main.java
+++ FideBurger/src/main/java/fideburger/Main.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package fideburger;
 
-/**
- *
- * @author sofiamora
- */
 public class Main {
 
     public static void main(String[] args) {
